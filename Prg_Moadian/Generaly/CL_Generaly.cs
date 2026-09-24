2d69da3a1b40922cf22bb4a2ce75a2e8a008e05e
mojtabahakimian <72439342+mojtabahakimian@users.noreply.github.com>
2026-09-24 11:13:45 +0330

    ver

--- Prg_Moadian/Generaly/CL_Generaly.cs
+++ Prg_Moadian/Generaly/CL_Generaly.cs
@@ -5,8 +5,8 @@ namespace Prg_Moadian.Generaly
 {
     public static class CL_Generaly
     {
-        public static string AppVersion = "9.0.0";
-        public static string AppDateShamsi = "1403/06/26";
+        public static string AppVersion = "9.0.1";
+        public static string AppDateShamsi = "1403/07/02";
         public static string AppVersionDisplay => $"نسخه {AppVersion} - {AppDateShamsi}";
 
         public static string MrCorrect { get; set; } = "m";
